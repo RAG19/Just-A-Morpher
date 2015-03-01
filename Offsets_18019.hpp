1163ea2b80f4849316235ce5b3b1e83246131a16
Saylance <bogdanpower@ukr.net>
2015-03-01 22:28:02 +0200

    More offsets for WoW 5.4.7 (18019)

--- Offsets_18019.hpp
+++ Offsets_18019.hpp
@@ -14,20 +14,20 @@ namespace JustAMorpher
 			enum : size_t
 			{
 				CGUnit_C__UpdateDisplayInfo = 0x42EC88,
-				CGUnit_C__OnMountDisplayChanged = 0x42E193, //??
-				CGUnit_C__UpdateScale = 0x424AE3, //??
+				CGUnit_C__OnMountDisplayChanged = 0x42EA73, // need to test -- 42EA29 -- 42ECCF -- 42E9DE -- 42EC9F
+				CGUnit_C__UpdateScale = 0x4253C3, // need to test -- 425114 --
 
-				GetUnitFromName = 0x4a04db,
-				FrameScript__InvalidPtrCheck = 0x110c83,
-				CGGameUI__Initialize = 0x8D317D, //??
+				GetUnitFromName = 0x4A04BD,
+				FrameScript__InvalidPtrCheck = 0x110C83,
+				CGGameUI__Initialize = 0x8D378C, // need to test
 
-				FrameScript__RegisterFunction = 0x04F2B7,
+				FrameScript__RegisterFunction = 0x4F2B7,
 				FrameScript__ExecuteBuffer = 0x50236,
 				lua_gettop = 0xD64F0,
-				luaD_error = 0xd7a92,
-				lua_tolstring = 0xd6a70,
-				lua_tonumber = 0xd69b7,
-				lua_pushnumber = 0xd6bb5,
+				luaD_error = 0xD7A92,
+				lua_tolstring = 0xD6A70,
+				lua_tonumber = 0xD69B7,
+				lua_pushnumber = 0xD6BB5,
 			};
 		}
 
@@ -54,13 +54,13 @@ namespace JustAMorpher
 		{
 			enum : size_t
 			{
-				MountDisplayID = 0x11C, //??
+				MountDisplayID = 0x11C,
 
-				Race = 0x5C, //??
-				Gender = 0x78, //??
-				DisplayID = 0x114, //??
-				NativeDisplayID = 0x118, //??
-				ItemDisplayIDs = 0x28, //??
+				Race = 0x5C, // need to test
+				Gender = 0x78,
+				DisplayID = 0x114,
+				NativeDisplayID = 0x118,
+				ItemDisplayIDs = 0x28, // need to test
 			};
 		}
 	}
